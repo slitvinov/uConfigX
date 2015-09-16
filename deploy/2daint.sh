716738c6beeb0d02847a991020315b2e7a43fd0d
Athena Economides <eceva@panda.ethz.ch>
2015-09-16 16:51:11 +0200

    Deploy on daint

---
+++ deploy/2daint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. utils/ucx/env.sh
+
+default_dir=uConfigX
+rname=test #= rname=%my_dir_name% 
+
+# remote host name
+uname=eceva
+rhost="${uname}"@daint
+
+# remote path name
+rpath=/scratch/daint/"${uname}"/RBCstretcing/"${rname}"
+
+#ur gcp "${default_dir}" "${rhost}":"${rpath}"
+
+# execute command remotely with <gitroot> as a current directory
+rt () {
+    ssh "${rhost}" "cd ${rpath}/${default_dir} ; $@"
+}
+
+rt local/daint/setup.sh
