c3cdffe8920b914aaa92dd4216e0b0022737af26
Athena Economides <eceva@panda.ethz.ch>
2015-09-16 19:24:38 +0200

    Twiking configuration

--- DOE/batch0.sh
+++ DOE/batch0.sh
@@ -2,11 +2,14 @@
 
 . utils/ucx/env.sh
 
+set -x
+
 tmp_d_file=`mktemp /tmp/batch0.XXXX`
 
 ur alcartesio.awk "$CART" > "$tmp_d_file"
 
-while read pline
+cat "$tmp_d_file" | while read pline
 do
+    echo "PLINE: " $pline
     ur aldriver2.sh $GITROOT $pline
-done < "$tmp_d_file"
+done
