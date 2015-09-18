91d6dcb9ff4a1f5725d3e7a42ffcc01710d109c7
Athena Economides <eceva@panda.ethz.ch>
2015-09-18 17:55:00 +0200

    Use ur in av_and_reduce

--- post/hdf5/av_and_reduce.sh
+++ post/hdf5/av_and_reduce.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PYTHON=$HOME/gerris-deploy/prefix/bin/python2.7
 out_h5=$1
 out_xmf=${out_h5/\.h5/.xmf}
 shift
@@ -7,5 +8,5 @@ shift
 fst_h5=$1
 fst_xmf=${fst_h5/\.h5/.xmf}
 
-python av_and_reduce.py $out_h5 "$@"
-./av_and_reduce.awk $out_xmf $fst_xmf
+$PYTHON `us av_and_reduce.py` $out_h5 "$@"
+ur av_and_reduce.awk $out_xmf $fst_xmf
