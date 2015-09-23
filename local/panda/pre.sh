39423cffced8c548445d802b5fe54b5867e4e3ca
Sergey Litvinov <slitvinov@gmail.com>
2015-09-23 15:25:35 +0200

    Add cell-template, copy cell template in preprocessor

--- local/panda/pre.sh
+++ local/panda/pre.sh
@@ -5,5 +5,8 @@
 # create cylinder geometry
 ur tsdf.awk pre/tsdf-configs/cylinder1.tsdf uDeviceX/mpi-dpd/sdf.dat
 
+# copy rbc template
+cp pre/cell-template/rbc.org.dat uDeviceX/cuda-rbc/rbc.dat
+
 # generate several RBCs
 ur cell-placement1.awk | sort -g | ur cell-placement0.awk -v phix=$phix >  uDeviceX/mpi-dpd/rbcs-ic.txt
