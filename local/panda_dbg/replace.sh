ae3c93cb7c9dd13f7840cead36b81a4a948caff4
Sergey Litvinov <slitvinov@gmail.com>
2015-11-18 16:36:41 +0100

    Clean up replace

--- local/panda_dbg/replace.sh
+++ local/panda_dbg/replace.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# do batch replace
+# do in-place batch replace in a list of files
 # Usage:
-# ./replace.sh A B [f1 ...]
+# ./replace.sh A B [file1 ...]
 
 old=$1; shift
 new=$1; shift
 
 t=`mktemp /tmp/rep.XXX`
 
-for f ; do
-    sed  -e "s,$old,$new,g" "$f" >  "$t"
+for f
+do
+    sed "s,$old,$new,g" "$f" >  "$t"
     mv "$t" "$f"
 done
